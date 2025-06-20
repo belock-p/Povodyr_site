e84d8167d97e4f63d0c0658c3f9dbb457a33db2c
Pavlo Bilokin <126197840+belock-p@users.noreply.github.com>
2025-06-20 22:08:19 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/Povodyr_site/', // <-- ADD THIS LINE
+  base: '/Povodyr_site/',
   server: {
     host: "::",
     port: 8080,
