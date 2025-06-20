cfd1940a6d701a88e16e9c01658320f3e04c99bf
Pavlo Bilokin <126197840+belock-p@users.noreply.github.com>
2025-06-20 22:30:35 +0300

    Update DetailsSection.tsx

--- src/components/DetailsSection.tsx
+++ src/components/DetailsSection.tsx
@@ -42,7 +42,7 @@ const DetailsSection = () => {
           <div className="rounded-2xl sm:rounded-3xl overflow-hidden shadow-elegant">
             {/* Card Header with background image instead of gradient */}
             <div className="relative h-48 sm:h-64 p-6 sm:p-8 flex items-end" style={{
-            backgroundImage: "url('/background-section3.png')",
+            backgroundImage: "url('/Povodyr_site/background-section3.png')",
             backgroundSize: "cover",
             backgroundPosition: "center"
           }}>
